f730ef383fc2e5787f64e09d284af4a62dcaef4f
Wagner Rodrigues <wra.silva@gmail.com>
2018-12-13 15:08:24 -0200

    feat(redis): test redis

--- main.go
+++ main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "github.com/gorilla/mux"
-    "net/http"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/", homeHandler)
-    http.Handle("/", r)
-    http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler).Methods("GET")
+	r.HandleFunc("/", redisHandler).Methods("POST")
+
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there")
+	fmt.Fprintf(w, "Hi there")
+}
+
+func redisHandler(w http.ResponseWriter, r *http.Request) {
+	ping()
+
+	fmt.Fprintf(w, "Redis %s", ping())
+}
+
+func ping() error {
+	client := NewClient()
+
+	pong, err := client.Ping().Result()
+	if err != nil {
+		return err
+	}
+	fmt.Println(pong, err)
+	// Output: PONG <nil>
+
+	return nil
 }
